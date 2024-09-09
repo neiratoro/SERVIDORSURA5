3f11800fd264f816dbcdc3f81f26eb26d2837bee
juanpablogilarango <dinosauriotedy@gmail.com>
2024-09-09 11:51:24 -0500

    se crea proyecto

---
+++ src/main/java/com/example/SEVIDORSURA5/Sevidorsura5Application.java
@@ -0,0 +1,13 @@
+package com.example.SEVIDORSURA5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sevidorsura5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sevidorsura5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SEVIDORSURA5/Sevidorsura5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SEVIDORSURA5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sevidorsura5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
